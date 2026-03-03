762d3bf16e9c260c33cec11572826e07ed35e793
ppoulincsf <ppoulin@csfoy.ca>
2026-03-03 17:06:24 -0500

    Initial commit

---
+++ Exercice05/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Exercice05
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      ReadNumber();
+      ReadUsernamePasswordLimitedAttempts();
+      GenerateRandomNumbers();
+      FindNumber();
+    }
+
+
+    public static void ReadNumber()
+    {
+      const int MIN = 0;
+      const int MAX = 10;
+
+      // A compléter
+    }
+
+    public static void ReadUsernamePasswordLimitedAttempts()
+    {
+      const string ACCEPTED_USER = "intro";
+      const string ACCEPTED_PASSWORD = "w10";
+      const int MAX_NUMBER_OF_ATTEMPTS = 3;
+
+      // A compléter
+    }
+
+    public static void GenerateRandomNumbers()
+    {
+      // Quantité de nombres à générer 
+      const int NB_TO_GENERATE = 3000;
+
+      // A compléter
+    }
+    public static void FindNumber()
+    {
+      const int MAXIMUM_VALUE = 100;
+
+      // A compléter
+    }
+
+  }
+}
